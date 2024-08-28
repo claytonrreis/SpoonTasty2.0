8171d9bb86d51a7fb2447fa020c12afe6f0304d8
claytonrreis <103720189+claytonrreis@users.noreply.github.com>
2024-08-28 15:29:04 -0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
+
+// cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// })
+
 app.use(express.json());
 app.use(cookieParser());
 

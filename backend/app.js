00728f6492e8f94593101221dd8fcb9df901a927
claytonrreis <103720189+claytonrreis@users.noreply.github.com>
2024-08-28 16:09:54 -0500

    last last try

--- backend/app.js
+++ backend/app.js
@@ -26,15 +26,18 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/spooners", spoonerRoutes);
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGO_URI
+    //   {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
-
-app.use("/api/spooners", spoonerRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

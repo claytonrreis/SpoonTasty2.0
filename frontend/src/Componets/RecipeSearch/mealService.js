72f37a48a79427d6938226b0fb4fb43bcf1e0fa6
claytonrreis <103720189+claytonrreis@users.noreply.github.com>
2024-08-29 10:42:35 -0500

    v14

--- frontend/src/Componets/RecipeSearch/mealService.js
+++ frontend/src/Componets/RecipeSearch/mealService.js
@@ -14,9 +14,26 @@
 //   }
 // };
 
+// import axios from "axios";
+
+// const API_URL = "/api/meals/";
+
+// export const searchMeals = async (query) => {
+//   try {
+//     const response = await axios.get(`${API_URL}search.php`, {
+//       params: { s: query },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching meals:", error);
+//     throw error;
+//   }
+// };
+
 import axios from "axios";
 
-const API_URL = "/api/meals/";
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://www.themealdb.com/api/json/v1/1/";
 
 export const searchMeals = async (query) => {
   try {
